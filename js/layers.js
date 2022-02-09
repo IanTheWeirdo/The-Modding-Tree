da129188131454f60ad1522be8b3fbf9141ee77a
Ian Mulraney <73891479+IanTheWeirdo@users.noreply.github.com>
2022-02-09 12:42:24 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -8,7 +8,7 @@ addLayer("p", {
     resource: "prestige points",            // The name of this layer's main prestige resource.
     row: 0,                                 // The row this layer is on (0 is the first row).
 
-    baseResource: "coins",                 // The name of the resource your prestige gain is based on.
+    baseResource: "points",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.points },  // A function to return the current amount of baseResource.
 
     requires: new Decimal(5),              // The amount of the base needed to  gain 1 of the prestige currency.
