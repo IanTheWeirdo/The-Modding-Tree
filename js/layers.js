d7c65fe0d8bea942153030755a77534ea712aa9b
Ian Mulraney <73891479+IanTheWeirdo@users.noreply.github.com>
2022-02-09 13:39:05 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,53 +1,28 @@
-addLayer("a", {
-    name: "Achievements", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-        points: new Decimal(0),
-    }},
-    tooltip() {
-      return "Achievements"
-    },
-    color: "#FFFF00",
-    nodeStyle() {return {
-        "background": "radial-gradient(#FFFF00, #d5ad83)" ,
-    }},
-    requires: new Decimal(0), // Can be a function that takes requirement increases into account
-    resource: "Achievement Points", // Name of prestige currency
-    type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    row: "side", // Row the layer is in on the tree (0 is the first row)
-    layerShown() { return true }, 
-})
-
 addLayer("p", {
-    startData() { return {                  // startData is a function that returns default data for a layer. 
-        unlocked: true,                     // You can add more variables here to add them to your layer.
-        points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
+    startData() { return {                  
+        unlocked: true,                     
+        points: new Decimal(0),           
     }},
 
-    color: "#4BDC13",                       // The color for this layer, which affects many elements.
-    resource: "prestige points",            // The name of this layer's main prestige resource.
-    row: 0,                                 // The row this layer is on (0 is the first row).
+    color: "#4BDC13",                       
+    resource: "prestige points",            
+    row: 0,                               
 
-    baseResource: "points",                 // The name of the resource your prestige gain is based on.
-    baseAmount() { return player.points },  // A function to return the current amount of baseResource.
+    baseResource: "points",                 
+    baseAmount() { return player.points },  
 
-    requires: new Decimal(10),              // The amount of the base needed to  gain 1 of the prestige currency.
-                                            // Also the amount required to unlock the layer.
+    requires: new Decimal(10),             
+    
+    type: "normal",                         
+    exponent: 0.5,                          
 
-    type: "normal",                         // Determines the formula used for calculating prestige currency.
-    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
-
-    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
-        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
+    gainMult() {                            
+        return new Decimal(1)               
     },
-    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
+    gainExp() {                             
         return new Decimal(1)
     },
 
-    layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
-
+    layerShown() { return true },          
     },
 })
