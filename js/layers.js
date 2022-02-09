ef4ec69809d051431332e002a90c3d414c66a0b5
Ian Mulraney <73891479+IanTheWeirdo@users.noreply.github.com>
2022-02-09 12:39:41 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,37 +1,30 @@
 addLayer("p", {
-    name: "Pennies", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
+    startData() { return {                  // startData is a function that returns default data for a layer. 
+        unlocked: true,                     // You can add more variables here to add them to your layer.
+        points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
     }},
-    color: "#4BDC13",
-    requires: new Decimal(5), // Can be a function that takes requirement increases into account
-    resource: "pennies", // Name of prestige currency
-    baseResource: "coins", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
+
+    color: "#4BDC13",                       // The color for this layer, which affects many elements.
+    resource: "pennies",            // The name of this layer's main prestige resource.
+    row: 0,                                 // The row this layer is on (0 is the first row).
+
+    baseResource: "coins",                 // The name of the resource your prestige gain is based on.
+    baseAmount() { return player.points },  // A function to return the current amount of baseResource.
+
+    requires: new Decimal(5),              // The amount of the base needed to  gain 1 of the prestige currency.
+                                            // Also the amount required to unlock the layer.
+
+    type: "normal",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0,                          // "normal" prestige gain is (currency^exponent).
+
+    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
+        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
     },
-    gainExp() { // Calculate the exponent on main currency from bonuses
+    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for pennies", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true}
-	upgrades: {
-    11: {
-	title: "Work Slightly Harder."
-        description: "+1 coin/s",
-        cost: new Decimal(10),
-	
+
+    layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
+
     },
-    
-}
 })
