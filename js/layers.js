86ef99258c2b49d3749145e608e7ffc1fdd0fc5b
Ian Mulraney <73891479+IanTheWeirdo@users.noreply.github.com>
2022-02-10 09:30:03 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,5 @@
-addLayer("p", {
-    name: "rocks", // This is optional, only used in a few places, If absent it just uses the layer id.
+addLayer("r", {
+    name: "Rocks", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -8,7 +8,7 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "rocks", // Name of prestige currency
+    resource: "Rocks", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -27,12 +27,13 @@ addLayer("p", {
 	layerShown(){return true}
 	
 	upgrades: {
-        rows: 3,
+        rows: 1,
         cols: 3,
         11: {
 		title: "The First of Many",
 		description: "Multiply point gain by 2",
 		cost: new Decimal(10),
-	}}	
+	},
+    }	
 })
 
